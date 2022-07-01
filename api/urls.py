2a0a90f772b79613082f1126181d941b637e9f49
seba3567 <sebastian26481@gmail.com>
2022-07-01 01:40:52 -0400

    api | delete fixed       put error

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path #importar libreria path
 from .views import PruebasViews #importar clase idusuarioViews
 
 urlpatterns = [
-    path('Datos', PruebasViews.as_view(), name='Datos_list'), #crear ruta index2
-]
+    path('Datos/', PruebasViews.as_view(), name='Datos_list'), #crear ruta index2
+    path('Datos/<int:idusuario>', PruebasViews.as_view(), name='companies_process')]
--- api/views.py
+++ api/views.py
@@ -18,13 +18,21 @@ class PruebasViews(View): #crear clase idusuarioViews
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request): #crear funcion get
-        Datos=list(Pruebas.objects.values()) #crear variable Datos
-        if len(Datos)>0:
-            datos={'message': 'Success', 'Datos': Datos} #crear variable datos
+    def get(self, request, idusuario=0): #crear funcion get
+        if (idusuario > 0): #si id_usuario es mayor a 0
+            Datos = list(Pruebas.objects.filter(idusuario=idusuario).values())
+            if len(Datos) > 0:
+                Dates = Datos[0]
+                datos={'message': 'Success', 'Datos': Dates}
+            else:
+                datos={'message': 'No hay datos'}
         else:
-            datos={'message': 'No hay datos'}
-        return JsonResponse(datos) #retornar datos
+            Datos=list(Pruebas.objects.values()) #crear variable Datos
+            if len(Datos)>0:
+                datos={'message': 'Success', 'Datos': Datos} #crear variable datos
+            else:
+                datos={'message': 'No hay datos'}
+            return JsonResponse(datos) #retornar datos
 
 
     def post(self, request): #crear funcion post
@@ -34,9 +42,32 @@ class PruebasViews(View): #crear clase idusuarioViews
         return JsonResponse (Dates) #retornar Dates
 
         
-    def put(self, request): #crear funcion put
-        pass
-    def delete(self, request): #crear funcion delete
-        pass
+    def put(self, request,idusuario): #crear funcion put
+        jd = json.loads(request.body) #crear variable jd
+        Datos = list(Pruebas.objects.filter(idusuario=idusuario).values()) #crear variable Datos
+        if len(Datos) > 0:
+            Datos=Pruebas.objects.get(idusuario=idusuario) #crear variable Datos
+            Datos.correo=jd['correo'] #crear variable Datos
+            Datos.nombre=jd['nombre'] #crear variable Datos
+            Datos.apellido=jd['apellido'] #crear variable Datos
+            Datos.rut=jd['rut'] #crear variable Datos
+            Datos.pwd=jd['pwd'] #crear variable Datos
+            Datos.save() #guardar Datos
+        else:
+            datos={'message': 'No hay datos'} #crear variable datos
+        return JsonResponse(datos) #retornar datos
+
+    
+    
+    
+    
+    def delete(self, request, idusuario): #crear funcion delete
+        Datos = list(Pruebas.objects.filter(idusuario=idusuario).values()) 
+        if len(Datos) > 0:
+            Pruebas.objects.filter(idusuario=idusuario).delete() #crear variable Datos
+            datos={'message': 'Success'} #crear variable datos
+        else:
+            datos={'message': 'No hay datos'}
+        return JsonResponse(datos) #retornar datos
 
 
